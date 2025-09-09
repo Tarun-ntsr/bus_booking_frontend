2ae007d6cd076b3885e0f808cebe37ab5e944d5d
Tarun-ntsr <tarun.reddy@innvolution.com>
2025-09-09 15:51:29 +0530

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    // if using src folder
-    // optional basePath if needed
-}
-
-module.exports = nextConfig
